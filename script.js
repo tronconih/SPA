7fdd131318631a8c2724085ae459fc788e5caa09
tronconih <hendrik.tronconi@studenti.iispascal.it>
2023-06-09 17:52:19 +0200

    Feat: adds new features for mobile users
    It adds new features for mobile users, but it's incomplete

--- script.js
+++ script.js
@@ -8,3 +8,46 @@ let w = window.innerWidth;
 //EVENTS
 let mobile = checkDevice();
 btnHome.onclick = previousPage;
+
+//FUNCTIONS
+function nextPage(){
+    divIntro.style.display = "none";
+}
+
+function previousPage(){
+    divIntro.style.display = "grid"; 
+}
+
+function checkDevice(){
+    if(/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)){
+        // true for mobile device
+        changeLayout();
+        return true;
+      }else{
+        // false for not mobile device
+        alert("desktop");
+        return false;
+    }
+}
+
+function changeLayout(){
+    alert("phone");
+    btnMenu.src = "images/menu.png";
+    btnMenu.style.zIndex = "10";
+    //MENU
+    menu.style.display = "block";
+    menu.style.position = "fixed";
+    menu.style.top = "-200px";
+    menu.style.right = "0";
+    menu.style.left = "0";
+    menu.style.transition = "top 1s"; // animation of the top that switch from -200 to 0 px
+    menu.style.backgroundColor = "lightblue";
+
+    btnMenu.onclick = menuMobile;
+    //GRID
+
+}
+
+function menuMobile(){
+    menu.style.top = "0";
+}
