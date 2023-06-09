0703be0db575b03f4417951ca10f79e9f67061b2
tronconih <hendrik.tronconi@studenti.iispascal.it>
2023-06-09 17:56:18 +0200

    Fix: fixes some problems and adds transitions
    It corrects some functions and adds them animations

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ let btnMenu = document.getElementById("btnMenu");
 let divIntro = document.getElementById("intro");
 let menu = document.getElementById("menu");
 let w = window.innerWidth;
+let hamburger = false; //boolean for the hamburger menu state
 //EVENTS
 let mobile = checkDevice();
 btnHome.onclick = previousPage;
@@ -31,17 +32,23 @@ function checkDevice(){
 }
 
 function changeLayout(){
-    alert("phone");
-    btnMenu.src = "images/menu.png";
+    //alert("phone");
+    hamburger = false;
+    btnMenu.src = "images/menu.svg";
     btnMenu.style.zIndex = "10";
+    btnMenu.style.position = "relative";
     //MENU
+    menu.style.marginTop = "0px";
     menu.style.display = "block";
     menu.style.position = "fixed";
-    menu.style.top = "-200px";
+    menu.style.top = "-15%";
     menu.style.right = "0";
     menu.style.left = "0";
-    menu.style.transition = "top 1s"; // animation of the top that switch from -200 to 0 px
     menu.style.backgroundColor = "lightblue";
+    menu.style.transition = "top 1s"; // animation of the top that switch from -200 to 0 px
+    btnMenu.style.transition = "rotate 0.5s"; // animation of the top that switch from -200 to 0 px
+    //assignments to reset the transition
+    btnMenu.style.rotate = "0deg"; 
 
     btnMenu.onclick = menuMobile;
     //GRID
