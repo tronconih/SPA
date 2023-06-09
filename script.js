88916331cd932c54b0a33eabca94b93f9e6a92c5
tronconih <hendrik.tronconi@studenti.iispascal.it>
2023-06-09 17:42:27 +0200

    Style: adds CSS and HTML structures
    It adds the whole CSS of the home and write down the HTML

---
+++ script.js
@@ -0,0 +1,7 @@
+//DECLARATIONS
+let btnContatti = document.getElementById("btnContacts");
+let btnHome = document.getElementById("home");
+let btnMenu = document.getElementById("btnMenu");
+let divIntro = document.getElementById("intro");
+let menu = document.getElementById("menu");
+let w = window.innerWidth;
