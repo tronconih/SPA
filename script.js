7d7909075dcd733e9142097e3cf6416c7f029ea3
tronconih <hendrik.tronconi@studenti.iispascal.it>
2023-06-12 16:06:58 +0200

    Fix: fixes the mobile's user menu
    It manages the mobile and desktop menu like usual but from css

--- script.js
+++ script.js
@@ -28,68 +28,23 @@ function previousPage(){
     divContacts.style.display = "none";
 }
 
-function checkDevice(){
-    if(/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)){
-        // true for mobile device
-        changeLayout();
-        return true;
-      }else{
-        // false for not mobile device
-        return false;
-    }
-}
-
-function changeLayout(){
-
-    hamburger = false;
-    btnMenu.src = "images/menu.svg";
-    btnMenu.style.zIndex = "10";
-    btnMenu.style.position = "relative";
-
-    //MENU
-    menu.style.marginTop = "0px";
-    menu.style.display = "block";
-    menu.style.position = "fixed";
-    menu.style.top = "-15%";
-    menu.style.right = "0";
-    menu.style.left = "0";
-    menu.style.zIndex = "5";
-    menu.style.backgroundColor = "lightblue";
-    menu.style.transition = "top 1s"; // animation of the top that switch from -200 to 0 px
-    btnMenu.style.transition = "rotate 0.5s"; // animation of the top that switch from -200 to 0 px
-    btnMenu.onclick = menuMobile;  
-    //assignments to reset the transition
-    btnMenu.style.rotate = "0deg"; 
-
-    //GRID 2
-    divGrill2.style.gridTemplateColumns = "50% 30%";
-    divGrill2.style.gridTemplateRows = "repeat(4, 50%[row-start])";
-    document.getElementById("contacts").style.marginTop = "20%";
-        //single area settings
-            //pic
-            pic.style.gridColumn = "1";
-            pic.style.gridRow = "1/4";
-
-            //name
-            nome.style.gridColumn = "2";
-            nome.style.gridRow = "1";
-            //email
-            email.style.gridColumn = "2";
-            email.style.gridRow = "2";
-            //phone number
-            phone.style.gridColumn = "2";
-            phone.style.gridRow = "3";
-            //socials
-            socials.style.gridColumn = "1/3";
-            socials.style.gridRow = "4";
-}
-
 function menuMobile(){
-    btnMenu.src = "images/cancel.svg";
-    btnMenu.style.rotate = "360deg";
-    menu.style.top = "0";
-    hamburger = true;
-    btnMenu.onclick = changeLayout;
-    //transition reset
-    btnMenu.style.transition = "rotate 0.5s";
+
+    if(hamburger != true){
+        menu.style.height= "100vh"; /* For 100% screen height */
+        btnMenu.style.content = "url(images/cancel.svg)";
+        btnMenu.style.rotate = "360deg";
+    menu.style.left = "0";
+        divContacts.style.visibility = "hidden";
+        divContacts.style.transition = "opacity 2s";
+        hamburger = true;
+    }
+    else{
+        menu.style.height= "0"; /* For 100% screen height */
+        btnMenu.style.content = "url(images/menu.svg)";
+    btnMenu.style.rotate = "0deg"; 
+        menu.style.left = "-100%";
+        divContacts.style.visibility = "visible";
+        hamburger = false;
+}
 }
