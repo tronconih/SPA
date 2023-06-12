664f33071a4328cf3fe7fc3df39f6e74b7fcc18e
tronconih <hendrik.tronconi@studenti.iispascal.it>
2023-06-12 10:14:31 +0200

    Style: fixes the second Grid
    It fixes the second grid for mobile users

--- script.js
+++ script.js
@@ -6,6 +6,12 @@ let divGrill2 = document.getElementById("grill2");
 let menu = document.getElementById("menu");
 let w = window.innerWidth;
 let hamburger = false; //boolean for the hamburger menu state
+let divGrill2 = document.getElementById("grill2");
+let pic = document.getElementById("myPic");
+let nome = document.getElementById("name");
+let email = document.getElementById("email");
+let phone = document.getElementById("number");
+let socials = document.getElementById("socials");
 divContacts.style.display = "none";
 >>>>>>> Stashed changes
 
@@ -76,25 +82,39 @@ function changeLayout(){
     menu.style.backgroundColor = "lightblue";
     menu.style.transition = "top 1s"; // animation of the top that switch from -200 to 0 px
     btnMenu.style.transition = "rotate 0.5s"; // animation of the top that switch from -200 to 0 px
+    btnMenu.onclick = menuMobile;  
     //assignments to reset the transition
     btnMenu.style.rotate = "0deg"; 
 
-    btnMenu.onclick = menuMobile;
-    //GRID
+    //GRID 1
 
+    //GRID 2
+    divGrill2.style.gridTemplateColumns = "50% 30%";
+    divGrill2.style.gridTemplateRows = "repeat(4, 50%[row-start])";
+    document.getElementById("contacts").style.marginTop = "20%";
+        //single area settings
+            //pic
+            pic.style.gridColumn = "1";
+            pic.style.gridRow = "1/4";
+
+            //name
+            nome.style.gridColumn = "2";
+            nome.style.gridRow = "1";
+            //email
+            email.style.gridColumn = "2";
+            email.style.gridRow = "2";
+            //phone number
+            phone.style.gridColumn = "2";
+            phone.style.gridRow = "3";
+            //socials
+            socials.style.gridColumn = "1/3";
+            socials.style.gridRow = "4";
 }
 
 function menuMobile(){
     btnMenu.src = "images/cancel.svg";
     btnMenu.style.rotate = "360deg";
     menu.style.top = "0";
-}
-=======
-let divContacts = document.getElementById("contacts");
-let menu = document.getElementById("menu");
-let w = window.innerWidth;
-divContacts.style.display = "none";
->>>>>>> Stashed changes
     hamburger = true;
     btnMenu.onclick = changeLayout;
     //transition reset
