38ce6920cf4c4a0c7936f6b19688208aa9b09426
tronconih <hendrik.tronconi@studenti.iispascal.it>
2023-06-13 12:14:28 +0200

    Style: corrects some style issues
    Corrects some issues in the style and not only, like name(class, var)

--- script.js
+++ script.js
@@ -1,23 +1,8 @@
-//DECLARATIONS
-let btnContatti = document.getElementById("btnContacts");
-let btnHome = document.getElementById("home");
-let btnMenu = document.getElementById("btnMenu");
-let divContacts = document.getElementById("contacts");
-let menu = document.getElementById("menu");
-let w = window.innerWidth;
-let hamburger = false; //boolean for the hamburger menu state
-let divGrill2 = document.getElementById("grill2");
-let pic = document.getElementById("myPic");
-let nome = document.getElementById("name");
-let email = document.getElementById("email");
-let phone = document.getElementById("number");
-let socials = document.getElementById("socials");
-divContacts.style.display = "none";
-
 //EVENTS
-let mobile = checkDevice();
-
+const btnHome = document.getElementById("home");
 btnHome.onclick = previousPage;
+const divContacts = document.getElementById("contacts");
+divContacts.style.display = "block";
 
 //FUNCTIONS
 function nextPage(){
@@ -28,23 +13,31 @@ function previousPage(){
     divContacts.style.display = "none";
 }
 
+const menuButtonRel = document.getElementById("menuButtonRel");
+const menu = document.getElementById("menu");
+//boolean for the hamburger menu state
+let isMobileMenuOpen = false; 
+const divGrill2 = document.getElementById("grill2");
+
 function menuMobile(){
 
-    if(hamburger != true){
-        menu.style.height= "100vh"; /* For 100% screen height */
-        btnMenu.style.content = "url(images/cancel.svg)";
-        btnMenu.style.rotate = "360deg";
-    menu.style.left = "0";
+    if(isMobileMenuOpen != true){
+        //For 100% screen height 
+        menu.style.height= "100vh"; 
+        menuButtonRel.style.content = "url(images/cancel.svg)";
+        menuButtonRel.style.rotate = "360deg";
+        menu.style.left = "0";
         divContacts.style.visibility = "hidden";
         divContacts.style.transition = "opacity 2s";
-        hamburger = true;
+        isMobileMenuOpen = true;
     }
     else{
-        menu.style.height= "0"; /* For 100% screen height */
-        btnMenu.style.content = "url(images/menu.svg)";
-    btnMenu.style.rotate = "0deg"; 
+         //For 100% screen height
+        menu.style.height= "0";
+        menuButtonRel.style.content = "url(images/menu.svg)";
+        menuButtonRel.style.rotate = "0deg"; 
         menu.style.left = "-100%";
         divContacts.style.visibility = "visible";
-        hamburger = false;
-}
+        isMobileMenuOpen = false;
+    }
 }
