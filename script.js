2f310a998a5f0ea74a95c97dbddc456412e4fb29
tronconih <hendrik.tronconi@studenti.iispascal.it>
2023-06-14 14:56:34 +0200

    Chore: updates the drawer
    Separates the drawer from the menu and add a scroll-bar

--- script.js
+++ script.js
@@ -3,6 +3,8 @@ const contactButtonRef = document.getElementById("contactButtonRef");
 contactButtonRef.onclick = nextPage;
 const homeLinkRef = document.getElementById("homeLinkRef");
 homeLinkRef.onclick = previousPage;
+const homeLinkDrawerRef = document.getElementById("homeLinkDrawerRef");
+homeLinkDrawerRef.onclick = previousPage;   
 const homeGridRef = document.getElementById("homeGridRef");
 
 //FUNCTIONS
@@ -14,29 +16,55 @@ function previousPage(){
     homeGridRef.style.display = "grid"; 
 }
 
-const menuButtonRef = document.getElementById("menuButtonRef");
+const drawerToggleRef = document.getElementById("drawerToggleRef");
 const homeHeaderRef= document.getElementById("homeHeaderRef");
+const homeDrawerRef= document.getElementById("homeDrawerRef");
 const citationHeadingRef = document.getElementById("citationHeadingRef");
-let isMobileMenuOpen = false; //boolean for the hamburger menu state
+let isMobileHeaderOpen = false; //boolean for the hamburger header state
 
-function menuMobile(){
+function headerMobile(){
 
-    if(isMobileMenuOpen != true){
-        homeHeaderRef.style.height= "100vh"; /* For 100% screen height */
-        menuButtonRef.style.content = "url(images/cancel.svg)";
-        menuButtonRef.style.rotate = "360deg";
-        homeHeaderRef.style.left = "0";
-        homeGridRef.style.visibility = "hidden";
-        homeGridRef.style.transition = "opacity 2s";
-        isMobileMenuOpen = true;
+    if(isMobileHeaderOpen != true){
+        openDrawer();
     }
     else{
-        homeHeaderRef.style.height= "0"; /* For 100% screen height */
-        menuButtonRef.style.content = "url(images/menu.svg)";
-        menuButtonRef.style.rotate = "0deg"; 
-        homeHeaderRef.style.left = "-100%";
-        homeGridRef.style.visibility = "visible";
-        isMobileMenuOpen = false;
+        closeDrawer();
+    }
+
 }
 
+function openDrawer(){
+    /* For 100% screen height */
+    homeDrawerRef.style.visibility = "visible"; 
+    homeDrawerRef.style.height= "100vh"; 
+    drawerToggleRef.style.content = "url(images/cancel.svg)";
+    drawerToggleRef.style.rotate = "360deg";
+    homeDrawerRef.style.left = "0";
+    document.getElementsByTagName("BODY")[0].style.overflowY = "hidden";
+    isMobileHeaderOpen = true;
+}
+
+function closeDrawer(){
+    /* For 100% screen height */
+    homeDrawerRef.style.height= "0"; 
+    drawerToggleRef.style.content = "url(images/menu.svg)";
+    drawerToggleRef.style.rotate = "0deg"; 
+    homeDrawerRef.style.left = "-100%";
+    document.getElementsByTagName("BODY")[0].style.overflowY = "auto";
+    isMobileHeaderOpen = false;
+}
+
+let w = window.outerWidth;
+//function created to manage the problem of the css priorities (js-CSS > css)
+function manageWindowResize(){
+    if(w > 900){
+        isMobileHeaderOpen = false;
+        document.getElementsByTagName("BODY")[0].style.overflowY = "auto";
+        closeDrawer();
+        homeDrawerRef.style.height= "50px";
+    }
+    else{
+        homeDrawerRef.style.height= "0";
+        homeGridRef.style.height= "auto";
+    }
 }
