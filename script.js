845c900dd6e38f1b3434a10a968658985c4b01b8
tronconih <hendrik.tronconi@studenti.iispascal.it>
2023-06-16 16:19:40 +0200

    Style: unifies home and contacts pages

--- script.js
+++ script.js
@@ -4,71 +4,55 @@ contactButtonRef.onclick = nextPage;
 const homeLinkRef = document.getElementById("homeLinkRef");
 homeLinkRef.onclick = previousPage;
 const homeLinkDrawerRef = document.getElementById("homeLinkDrawerRef");
-homeLinkDrawerRef.onclick = previousPage;   
-const homeGridRef = document.getElementById("homeGridRef");
-const divContacts = document.getElementById("contacts");
-divContacts.style.display = "block";
+homeLinkDrawerRef.onclick = previousPage;
+const homeWrapperRef = document.getElementById("homeWrapperRef");
+const contactWrapperRef = document.getElementById("contactWrapperRef");
+contactWrapperRef.style.display = "none";
+//boolean for the hamburger header state
+let isMobileDrawerOpen = false;
 
 //FUNCTIONS
-function nextPage(){
-    homeGridRef.style.display = "none";
-    divContacts.style.display = "grid";
+function nextPage() {
+    homeWrapperRef.style.display = "none";
+    contactWrapperRef.style.display = "grid";
 }
 
-function previousPage(){
-    homeGridRef.style.display = "grid"; 
-    divContacts.style.display = "none";
+function previousPage() {
+    homeWrapperRef.style.display = "grid";
+    contactWrapperRef.style.display = "none";
+    if (isMobileDrawerOpen) closeDrawer();
 }
 
 const drawerToggleRef = document.getElementById("drawerToggleRef");
-const homeHeaderRef= document.getElementById("homeHeaderRef");
-const homeDrawerRef= document.getElementById("homeDrawerRef");
+const homeHeaderRef = document.getElementById("homeHeaderRef");
+const homeDrawerRef = document.getElementById("homeDrawerRef");
 const citationHeadingRef = document.getElementById("citationHeadingRef");
-let isMobileHeaderOpen = false; //boolean for the hamburger header state
 
-function headerMobile(){
-
-    if(isMobileHeaderOpen != true){
-        openDrawer();
-    }
-    else{
-        closeDrawer();
-    }
-
+function drawerMobile() {
+    !isMobileDrawerOpen  ? openDrawer() : closeDrawer();
 }
 
-function openDrawer(){
-    /* For 100% screen height */
-    homeDrawerRef.style.visibility = "visible"; 
-    homeDrawerRef.style.height= "100vh"; 
+function openDrawer() {
     drawerToggleRef.style.content = "url(images/cancel.svg)";
     drawerToggleRef.style.rotate = "360deg";
     homeDrawerRef.style.left = "0";
-    document.getElementsByTagName("BODY")[0].style.overflowY = "hidden";
-    isMobileHeaderOpen = true;
+    isMobileDrawerOpen = true;
 }
 
-function closeDrawer(){
+function closeDrawer() {
     /* For 100% screen height */
-    homeDrawerRef.style.height= "0"; 
     drawerToggleRef.style.content = "url(images/menu.svg)";
-    drawerToggleRef.style.rotate = "0deg"; 
-    homeDrawerRef.style.left = "-100%";
-    document.getElementsByTagName("BODY")[0].style.overflowY = "auto";
-    isMobileHeaderOpen = false;
+    drawerToggleRef.style.rotate = "0deg";
+    homeDrawerRef.style.left = "-110vw";
+    isMobileDrawerOpen = false;
 }
 
-let w = window.outerWidth;
 //function created to manage the problem of the css priorities (js-CSS > css)
-function manageWindowResize(){
-    if(w > 900){
-        isMobileHeaderOpen = false;
-        document.getElementsByTagName("BODY")[0].style.overflowY = "auto";
+function manageWindowResize() {
+    if (window.outerWidth > 900){
+        homeDrawerRef.style.display = "none";
         closeDrawer();
-        homeDrawerRef.style.height= "50px";
-    }
-    else{
-        homeDrawerRef.style.height= "0";
-        homeGridRef.style.height= "auto";
-    }
+        drawerToggleRef.style.content = "url(images/menu.svg)";
+    } 
+    else homeDrawerRef.style.display = "flex";
 }
