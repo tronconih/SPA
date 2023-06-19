15e74814e795ad8367e33d6376254a5eacfcacac
tronconih <hendrik.tronconi@studenti.iispascal.it>
2023-06-19 16:44:12 +0200

    Style: moves some buttons css in the web component

--- abstractbutton.js
+++ abstractbutton.js
@@ -1,5 +1,16 @@
 const template = document.createElement("template");
-template.innerHTML = "<slot name='home-grid-button'> Nothing were written in this slot </slot>";
+template.innerHTML = "<style>"+
+".home-button {"+
+    "backdrop-filter: blur(15px);"+
+    "border-radius: 25px;"+
+    "/* nullifies the style of buttons's borders */"+
+    "border-style: none;"+
+    "grid-column: 2;"+
+    "height: 120px;"+
+    "transition: all 0.9s;"+
+  "}"+
+"</style>"+
+"<slot name='home-grid-button'> Nothing were written in this slot </slot>";
 
 class AbstractButton extends HTMLElement{
     constructor(){
@@ -7,7 +18,7 @@ class AbstractButton extends HTMLElement{
         //container that is going to hold the component
         /* mode open = the parent script (main.js) can get inside our custom component and see everything
            mode close = the parent script (main.js) CAN'T get inside our custom component*/
-        const shadowRoot = this.attachShadow({mode: "closed"}); 
+        const shadowRoot = this.attachShadow({mode: "open"}); 
         let clone = template.content.cloneNode(true); 
         shadowRoot.append(clone);
     }
